9d7b1a8c4c9276b74a00a5fcdf6583873973e7ed
Lucas Lobell <lucas.lobell@logdom.logicainfo.com.br>
2024-08-13 16:45:30 -0300

    products, inventory, users and settings

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 /* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -20,6 +22,8 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes);
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 /* SEREVER */
 const port = process.env.PORT || 3001;
